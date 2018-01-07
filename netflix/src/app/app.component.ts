ca722b2c3a33b9b10236698212e95e20df11bf51
jEstevezRod <32360649+jEstevezRod@users.noreply.github.com>
2018-01-07 22:27:29 +0100

    update

--- netflix/src/app/app.component.ts
+++ netflix/src/app/app.component.ts
@@ -7,8 +7,18 @@ import { Observable } from 'rxjs/Rx';
 @Component({
   selector: 'app-root',
   template: `
-  <label for="search">Busqueda: </label>
-    <input id="search" type=text value="" #searcher (change)="buscar($event.target.value)">
+  <label id="titulo" for="search">
+    <span class="red">b</span>
+    <span class="green">u</span>
+    <span class="yellow">s</span>
+    <span class="red">Q</span>
+    <span class="green">u</span>
+    <span class="yellow">e</span>
+    <span class="red">d</span>
+    <span class="green">a</span>
+    <span id=peque>:</span> 
+  </label>
+  <input type="text" id="search"  value="" #searcher (change)="buscar($event.target.value)">
   <section id="canvas" infiniteScroll
   [infiniteScrollDistance]="1"
   [infiniteScrollThrottle]="50"
@@ -24,41 +34,63 @@ export class AppComponent {
   public title = "";
   public movies;
   public page = 1;
-  public globalArray=[];
+  public globalArray = [];
+  public loading = false;
+  public input;
+  public search;
+  public button;
+
 
   buscar($event) {
     this.globalArray = []
-    this.page=1;
+    this.page = 1;
     this.title = $event;
     this.Master();
   }
 
   constructor(public _GlobalPetitionService: GlobalPetitionService) {
-    
+
   }
 
   ngOnInit() {
+    var input = document.querySelector('.search-form');
+    var search = document.querySelector('input')
+    var button = document.querySelector('button');
+    button.addEventListener('click', function(e) {
+      e.preventDefault();
+      input.classList.toggle('active');
+    })
+    search.addEventListener('focus', function() {
+      input.classList.add('focus');
+    })
     
+    search.addEventListener('blur', function() {
+      search.value.length != 0 ? input.classList.add('focus') : input.classList.remove('focus');
+    })
   }
 
-  Master(){
+  Master() {
+    this.loading = true;
     this._GlobalPetitionService.petition(this.title, this.page).subscribe(
       data => {
         this.movies = data;
         this.movies = Array.of(this.movies);
-        for ( let movie of this.movies[0].Search){
+        for (let movie of this.movies[0].Search) {
           this.globalArray.push(movie);
         }
       },
-      err => console.error("NEEEEEEEEEEEEENG",err),
-      () => console.log('done loading movies')
+      err => console.error("NEEEEEEEEEEEEENG", err),
+      () => this.loading = false
     );
 
   }
 
-  nextPage(){
-    this.page++;
-    this.Master();
-
+  nextPage() {
+    if (!this.loading) {
+      this.page++;
+      this.Master();
+    }
   }
+
+
 }
